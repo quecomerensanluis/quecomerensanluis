a23a743d6ba1585918b46aed33fd7e2ccab1c997
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-04-06 00:52:07 -0300

    orderMSG

--- quecomerensanluis/src/app/carnes/parrilla_roca/page.js
+++ quecomerensanluis/src/app/carnes/parrilla_roca/page.js
@@ -170,6 +170,150 @@ const ParrillaRoca = () => {
       description: 'Opciones: Coca-Cola, Sprite, Fanta',
       price: 500,
     },
+    {
+      id: 'empanasdfdas',
+      name: 'Media Docena De Empanadas De Carne Al Horno',
+      description: '6 unidades de tapa de masa fresca con carne molida, cebolla, huevo, especias.',
+      price: 3000,
+    },
+    {
+      id: 'hamburguffsesa',
+      name: 'Hamburguesa Completa',
+      description: 'Carne, queso, lechuga, tomate, huevo, bacon y salsas',
+      price: 2500,
+    },
+    {
+      id: 'gaseogfgsa',
+      name: 'Gaseosa 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseosafhh 2',
+      name: 'Gaseosa 2  500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseoshhhga 3',
+      name: 'Gaseosa 3 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseofghsa 4',
+      name: 'Gaseosa  4 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gasedgosa 5',
+      name: 'Gaseosa  5 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gasesdfsa 6 ',
+      name: 'Gaseosa 6 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'empahgfnadas',
+      name: 'Meddfia Docfena De Empanadas De Carne Al Horno',
+      description: '6 unidades de tapa de masa fresca con carne molida, cebolla, huevo, especias.',
+      price: 3000,
+    },
+    {
+      id: 'hambuytrrguesa',
+      name: 'Hamburghuesa Comhgpleta',
+      description: 'Carne, queso, lechuga, tomate, huevo, bacon y salsas',
+      price: 2500,
+    },
+    {
+      id: 'gacvbseosa',
+      name: 'Gaseogfsa 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gasencosa 2',
+      name: 'Gasffeosa 2  500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseobnfsa 3',
+      name: 'Gaseosa 3 50dd0ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseonhmsa 4',
+      name: 'Gaseofgfsa  4 50f0ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseobnmmsa 5',
+      name: 'Gaseosafgga  5 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseosmmnna 6 ',
+      name: 'Gaseasfosa 6 500dssdaml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'empanadbnas',
+      name: 'Media Docesddsna De Empanadas De Carne Al Horno',
+      description: '6 unidades de tapa de masa fresca con carne molida, cebolla, huevo, especias.',
+      price: 3000,
+    },
+    {
+      id: 'hamburmbnguesa',
+      name: 'Hamburssguesa Comdpleta',
+      description: 'Carne, queso, lechuga, tomate, huevo, bacon y salsas',
+      price: 2500,
+    },
+    {
+      id: 'gasemnbosa',
+      name: 'Gaffseosa 500dsml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseonbvsa 2',
+      name: 'Gaseossa 2  500dml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseo bsa 3',
+      name: 'Gaseogsa 3 5g00ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseoshha 4',
+      name: 'Gaseofasa  4 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseoshha 5',
+      name: 'Gasedsosa  5 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
+    {
+      id: 'gaseosa 6 ',
+      name: 'Gaseossa 6 500ml',
+      description: 'Opciones: Coca-Cola, Sprite, Fanta',
+      price: 500,
+    },
 
   ];
 
@@ -178,9 +322,9 @@ const ParrillaRoca = () => {
     
     <main className="bg-indigo-600 flex flex-col min-h-screen rounded-xl border-8 outline-offset-2 border-slate-500 p-5">
       {/* ... */}
-      <section className="flex items-center justify-center flex-wrap gap-7 mb-12">
+      <section className="flex items-center justify-center flex-wrap gap-2 mb-64">
         {dishes.map((dish) => (
-          <div key={dish.id} className="mx-1">
+          <div key={dish.id} className="mx-1 ">
             <button className="flex flex-col gap-1 px-2 py-1 text-white duration-500 bg-red-800 rounded-lg border border-red-200 hover:bg-red-900 active:bg-indigo-700">
               <h2 className="text-left text-base mb-0.5">{dish.name}</h2>
               <h2 className="text-left text-sm mb-0.5">{dish.description}</h2>
@@ -199,30 +343,15 @@ const ParrillaRoca = () => {
 
 
       {/* Display the selected quantities */}
-
-      <div className="text-white bg-green-500 mb-5">
-         
-        {Object.entries(selectedQuantities).map(([dishId, quantity]) => (
-          quantity > 0 && ( // Only render if quantity is greater than 0
-            <p key={dishId}>
-              {dishes.find((dish) => dish.id === dishId)?.name}: {quantity}
-               
-            </p>
-            
- 
-             
-
-
-          )
-        ))}
-      </div>
+      <section className="  mb-64">
+      </section>
       
  
       {orderMessage && (
-        <div className="flex flex-col bg-white rounded-md shadow-md p-5 mt-auto">
+        <div className="fixed left-0 right-0 bottom-0 flex flex-col bg-sky-500 rounded-md shadow-md p-2 mt-5">
           {/* Order Message */}
-          <div className="mb-2 text-red">
-            <pre className="  text-red ">{orderMessage}</pre>
+          <div className="mb-2 flex items-center justify-center">
+            <pre >{orderMessage}</pre>
           </div>
           {/* Conditionally Render Send Order Button */}
           {Object.values(selectedQuantities).some((quantity) => quantity > 0) && (
